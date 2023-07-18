5f0c30407b6e89aff71bb1686e5036140e4ed9ae
Marcin Nowacki <38333038+Myszu@users.noreply.github.com>
2023-07-18 06:53:22 +0200

    Create progress_bar.py

---
+++ progress_bar.py
@@ -0,0 +1,32 @@
+from datetime import datetime, timedelta
+# NUMBER OF SEGMENTS
+LENGTH = 25
+
+def progress(step: int, steps: int, errors: list = [], delta: int = 0):
+    """Generates a progress bar in terminal for iterations.
+
+    Args:
+        step (int): Current iteration.
+        steps (int): Max number of iterations.
+        errors (list, optional): List of errors to count and print out (NYI) in the end. Defaults to [].
+        delta (int, optional): Timed average of iteration. Defaults to 0.
+    """
+    if step > steps or steps <= 0:
+        print(f'Progress bar overflown!')
+        return
+    percent = round((step/steps)*LENGTH)
+    eta = ''
+    if delta:
+        eta = f' ETA: {(datetime.now() + timedelta(seconds=(delta*(steps-step)))).strftime("%H:%M:%S")}'
+    if not errors:
+        if step < steps:
+            print(f"[{'█' * percent}{'-' * (LENGTH-percent)}] {int(round(step/steps, 2)*100)}% ({step}/{steps}){eta}", end='\r')
+            return
+        print(f"[{'█' * percent}{'-' * (LENGTH-percent)}] {int(round(step/steps, 2)*100)}% ({step}/{steps}){eta}")
+        return
+    else:
+        if step < steps:
+            print(f"[{'█' * percent}{'-' * (LENGTH-percent)}] {int(round(step/steps, 2)*100)}% ({step}/{steps}){eta} [Err: {len(errors)}, {round(len(errors)/steps, 2)*100}%]", end='\r')
+            return
+        print(f"[{'█' * percent}{'-' * (LENGTH-percent)}] {int(round(step/steps, 2)*100)}% ({step}/{steps}){eta} [Err: {len(errors)}, {round(len(errors)/steps, 2)*100}%]")
+        return
